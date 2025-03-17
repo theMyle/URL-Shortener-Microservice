9dceadd82cdf5d2fa86a958125975e2caf59c5c0
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-03-17 21:04:32 +0800

    finished'

--- index.js
+++ index.js
@@ -2,21 +2,52 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const { count } = require('console');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended: true})); // Post parsing middleware for parsing request body
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function(_, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Logic for shortening link
+let links = {};
+let counter = 1;
+
+app.post('/api/shorturl', (req, res) => {
+  let url = req.body.url;
+  let hostname = url;
+  
+  // check if url is valid
+  try {
+    let hostname = new URL(url).hostname;
+    dns.lookup(hostname, (err, address, family) => {
+      // store into a dictionary
+      links[counter] = url;               
+
+      // return json response
+      res.json({ original_url: url, 
+        short_url: counter });            
+
+      // increment counter for next one
+      counter++;                          
+    });
+  } catch {
+    res.json({error: 'invalid url'});
+  }
+});
+
+// Redirect to original url
+app.get('/api/shorturl/:short_url', (req, res) => {
+  let id = Number(req.params.short_url);
+  res.redirect(links[id]);
 });
 
 app.listen(port, function() {
